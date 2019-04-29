a1d824cbd6bd4478350cb196ca2a1f72b7202e55
HaneenWally <livetour1d>
2019-04-29 17:33:27 +0200

    Mary and Haneen merge

---
+++ lib/User/homePage.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+//import '../ChatRoom/chat_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import "dart.io";
+//import 'package:web_socket_channel/web_socket_channel.dart';
+//import 'package:web_socket_channel/io.dart';
+//import 'package:web_socket';
+
+/*class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(appBar: new AppBar(
+      title: new Text("Frenzy Chat"),
+    ),
+        body: new ChatScreen()
+    );
+
+  }
+}*/
+
+//Haneen
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(appBar: new AppBar(
+      title: new Text('HomePage'),
+    ),
+        body: new ChatRoomsList()
+    );
+
+  }
+}
+
+class ChatRoomsList extends StatefulWidget{
+  @override
+
+  _ChatRoomsListState createState(){
+    return _ChatRoomsListState();
+  }
+}
+
+class _ChatRoomsListState extends State<ChatRoomsList>{
+  @override
+
+  Widget build(BuildContext context){
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('chatrooms').snapshots(),
+      builder: (context, snapshot){
+        if(!snapshot.hasData) return LinearProgressIndicator();
+
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot){
+
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.admin),
+          onTap: () => print(record),
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String name;
+  final String admin;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['admin'] != null),
+        assert(map['name'] != null),
+        name = map['name'],
+        admin = map['admin'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import './SignUp/loginsignup.dart';
-
+import 'User/homePage.dart';
 
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+//Haneen main
+/*class MyApp() extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(title: 'HomePage', home: new HomePage());
+  }
+}
+*/
+
+//Mary main
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(title: "Chat App", home: new HomePage());
+
+  }
+
+}
+
+//Hagar main
+/*class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,4 +38,4 @@ class MyApp extends StatelessWidget {
       home: LoginSignUpPage(),//MyHomePage(),
     );
   }
-}
+}*/
